35fd54936fd0aa32b0fe580c89b228694f1fecd0
Giuliana De Bellis <z5309331@unsw.edu.au>
2022-12-07 11:59:33 +1100

    datastore

--- src/dataStore.js
+++ src/dataStore.js
@@ -17,12 +17,15 @@ Example usage
     setData(store)
 */
 
-// Use get() to access the data
+// Use getData() to access the data
 function getData() {
   return data;
 }
 
 // Use set(newData) to pass in the entire data object, with modifications made
+// - Only needs to be used if you replace the data store entirely
+// - Javascript uses pass-by-reference for objects... read more here: https://stackoverflow.com/questions/13104494/does-javascript-pass-by-reference
+// Hint: this function might be useful to edit in iteration 2
 function setData(newData) {
   data = newData;
 }
