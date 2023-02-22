ce2dc07a0024e3d01b06be3be740e9d6f80a35bb
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-02-22 19:49:27 +1100

    Changed to 2 space indentation

--- src/channels.js
+++ src/channels.js
@@ -1,12 +1,12 @@
 // Function lists details of all channels the user is in
 
 function channelsListAllV1 (authUserId) {
-    return {
-        channels: [
-            {
-                channelId: 1,
-                name: 'My Channel',
-            }
-        ]
-    }
+  return {
+    channels: [
+      {
+        channelId: 1,
+        name: 'My Channel',
+      }
+    ]
+  }
 }
