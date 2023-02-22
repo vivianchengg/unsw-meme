a9a99cbef25c4ed2306dff0b017b4d68a6722dac
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-02-22 19:45:23 +1100

    Changed to 2 space indentation

--- src/channel.js
+++ src/channel.js
@@ -1,25 +1,25 @@
 // Function that lists details of members in the channel
 
 function channelDetailsV1 (authUserId, channelId) {
-    return {
-        name: 'Hayden',
-        ownerMembers: [
-            {
-            uId: 1,
-            email: 'example@gmail.com',
-            nameFirst: 'Hayden',
-            nameLast: 'Jacobs',
-            handleStr: 'haydenjacobs',
-            }
-        ],
-        allMembers: [
-            {
-            uId: 1,
-            email: 'example@gmail.com',
-            nameFirst: 'Hayden',
-            nameLast: 'Jacobs',
-            handleStr: 'haydenjacobs',
-            }
-        ],
-    }
+  return {
+    name: 'Hayden',
+    ownerMembers: [
+      {
+          uId: 1,
+          email: 'example@gmail.com',
+          nameFirst: 'Hayden',
+          nameLast: 'Jacobs',
+          handleStr: 'haydenjacobs',
+      }
+    ],
+    allMembers: [
+      {
+        uId: 1,
+        email: 'example@gmail.com',
+        nameFirst: 'Hayden',
+        nameLast: 'Jacobs',
+        handleStr: 'haydenjacobs',
+      }
+    ],
+  }
 }
