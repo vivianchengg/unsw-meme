436316ca42240f2222eab2f2dc4a843486ef6ff5
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-02-23 12:53:37 +1100

    Fixed to 2 space indentation

--- src/channel.js
+++ src/channel.js
@@ -13,18 +13,19 @@ function channelInviteV1(authUserId, channelId, uId) {
 
 //channelMessagesV1 stub function
 function channelMessagesV1(authUserId, channelId, start) {
-    return {
-        messages: [
-            {
-                messageId: 1,
-                uId: 1,
-                message: 'Hello world',
-                timeSent: 1582426789,
-            }
+  return {
+    messages: [
+      {  
+        messageId: 1,
+        uId: 1,
+        message: 'Hello world',
+        timeSent: 1582426789,
+      }
     ],
     start: 0,
     end: 50,
-    }             
+  }             
 }
+
 
 
