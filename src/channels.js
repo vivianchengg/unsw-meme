8992890d8622767b2261a80e61cc0b18e6a6acf2
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-02-24 09:34:33 +1100

    Fixed spacing of functions

--- src/channels.js
+++ src/channels.js
@@ -1,10 +1,10 @@
 // stub function: creates a channel - returns a channel id
-function channelsCreateV1(authUserId, name, isPublic) {
+function channelsCreateV1 (authUserId, name, isPublic) {
   return {channelId: 1}
 }
 
-// stub function: creates a list of channels - returns channelId, name
-function channelsListV1(authUserId) {
+// stub function: creates a list of channels - returns channelId, name 
+function channelsListV1 (authUserId) {
   return {
     channels: [
       {
