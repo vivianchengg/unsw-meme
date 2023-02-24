deae6d46167937484faa1cf33aad243844562d47
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-02-24 12:26:46 +1100

    Added all channels functions into channels.js

--- src/channels.js
+++ src/channels.js
@@ -14,3 +14,16 @@ function channelsListV1 (authUserId) {
     ],
   }
 }
+
+// Function lists details of all channels the user is in
+
+function channelsListAllV1 (authUserId) {
+  return {
+    channels: [
+      {
+        channelId: 1,
+        name: 'My Channel',
+      }
+    ]
+  }
+}
