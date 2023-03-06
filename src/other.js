b5a7bc1a5398fde754aad0191a89dbfa469e76df
Vivian <z5332415@ad.unsw.edu.au>
2023-03-06 22:37:00 +1100

    update documentation

--- src/other.js
+++ src/other.js
@@ -1,5 +1,11 @@
 import { getData, setData } from './dataStore.js';
 
+/**
+  * Resets the internal data of the application to its initial state
+  * 
+  * @param {}  
+  * @returns {}
+*/
 export function clearV1() {
     const data = getData();
     data.users = [];
