ff916550aef4ac20bebdf1306348a63f6366b59c
Vivian <z5332415@ad.unsw.edu.au>
2023-03-07 19:21:57 +1100

    implement authLoginV1 function

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,49 @@
-// stub for authLoginV1
-function authLoginV1(email, password) {
+import { getData } from './dataStore.js';
+
+/**
+  * check whether email entered belong to a user
+  * 
+  * @param {string} email 
+  * @returns {bool}
+*/
+function isEmailFromUser(email) {
+  const data = getData();
+  let user = undefined;
+  if (data.users !== undefined) {
+    user = data.users.find(a => a.email === email);
+  }
+
+  if (user === undefined) {
+    return false;
+  } 
+  return true;
+}
+
+
+/**
+  * Given a registered user's email and password, returns their authUserId value.
+  * 
+  * @param {string} email 
+  * @param {string} password 
+  * @returns {{authUserId: number}}
+*/
+export function authLoginV1(email, password) {
+  const data = getData();
+  const user = data.users.find(a => a.email === email);
+
+  // error: email entered does not belong to a user or incorrect password
+  if (!isEmailFromUser(email)) {
+    return { error: 'invalid email' };
+  }
+
+  if (user.password !== password) {
+    return { error: 'incorrect password' };
+  }
+
+  const id = user.uId;
+
   return {
-    authUserId: 1,
+    authUserId: id,
   }
 }
 
@@ -11,4 +53,3 @@ function authRegisterV1(email, password, nameFirst, nameLast) {
     authUserId: 1,
   }
 }
-
