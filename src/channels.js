4b073f94d56f31a95eea06ed42ca9d872a52c3b7
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-08 19:22:16 +1100

    Fixed channels.js to display all channels

--- src/channels.js
+++ src/channels.js
@@ -17,7 +17,7 @@ function channelsListV1 (authUserId) {
   }
 }
 
-/** Function lists details of all channels the user is in
+/** Function lists details of all channels
  * 
  * @param {number} authUserId - User ID of individual calling function
  * @returns {channels: [{
@@ -39,14 +39,12 @@ function channelsListAllV1 (authUserId) {
 
   let channels_list = [];
   for (const channel of data.channels) {
-    if (channel_member(channel, authUserId) === true) {
       const channel_details = {
         channelId: channel.channelId,
         name: channel.name
-      };
+      }
 
       channels_list.push(channel_details);
-    }
   }
 
   return channels_list;
@@ -60,38 +58,6 @@ function validate_user (user_id) {
   const data = getData();
   for (const user of data.users) {
     if (user.userId === user_id) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-/** Function that checks if user is member of given channel
- *  @param {channel, number}
- *  @returns {boolean}
- *  
- * Here, channel: {
- *  channelId: number,
- *  name: string,
- *  isPublic: boolean,
- *  ownerMembers: user[],
- *  allMembers: user[]
- * }
- * 
- * user: {
- *  userId: number,
- *  email: string,
- *  password: string,
- *  nameFirst: string,
- *  nameLast: string,
- *  handleStr: string,
- * 
- * }
-*/
-function channel_member (channel, user_id) {
-  for (const member of channel.allMembers) {
-    if (member.userId === user_id) {
       return true;
     }
   }
