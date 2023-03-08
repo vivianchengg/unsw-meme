8701b181b00b27a875dd105e356d0dcb1bf69319
Vivian <z5332415@ad.unsw.edu.au>
2023-03-08 18:56:55 +1100

    modify error msg

--- src/auth.test.js
+++ src/auth.test.js
@@ -1,21 +1,23 @@
 import { authRegisterV1, authLoginV1 } from './auth.js';
 import { clearV1 } from './other.js';
 
+const ERROR = { error: expect.any(String) };
+
 beforeEach(() => {
   clearV1();
 });
 
 describe('authLoginV1 Test', () => {
   test('email entered does not belong to a user', () => {
-    expect(authLoginV1('vc@unsw.edu.au', 'password')).toStrictEqual({ error: 'invalid email' });
+    expect(authLoginV1('vc@unsw.edu.au', 'password')).toStrictEqual(ERROR);
 
     authRegisterV1('vc@unsw.edu.au', 'password', 'Vivian', 'Cheng');
-    expect(authLoginV1('vc1@unsw.edu.au', 'password')).toStrictEqual({ error: 'invalid email' });
+    expect(authLoginV1('vc1@unsw.edu.au', 'password')).toStrictEqual(ERROR);
   });
 
   test('password is not correct', () => {
     authRegisterV1('vc@unsw.edu.au', 'password', 'Vivian', 'Cheng');
-    expect(authLoginV1('vc@unsw.edu.au', 'pwd')).toStrictEqual({ error: 'incorrect password' });
+    expect(authLoginV1('vc@unsw.edu.au', 'pwd')).toStrictEqual(ERROR);
   });
 
   test('test login', () => {
