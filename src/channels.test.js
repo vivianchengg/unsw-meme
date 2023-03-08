1669af5bfe8db718ae559fc65c4ddbb8bc78c9bd
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-08 19:28:50 +1100

    Added more tests

--- src/channels.test.js
+++ src/channels.test.js
@@ -6,17 +6,48 @@ import { clearV1 } from './other.js';
 beforeEach(() => {
   clearV1();
   let user_id = authRegisterV1('jr@unsw.edu.au', 'password', 'Jake', 'Renzella');
-  let channel_id = channelsCreateV1(user_id, 'COMP1531', true);
+  const channel_id = channelsCreateV1(user_id, 'COMP1531', true);
+  const second_channel_id = channelsCreateV1(user_id, 'COMP2511', true);
 });
 
 describe('channelListAllV1 Tests'), () => {
   test('Invalid authUserId'), () => {
-    expect(channelsListAllV1(user_id + 1, channel_id)).toStrictEqual({ error: expect.any(String)});
+    expect(channelsListAllV1(user_id + 1)).toStrictEqual({ error: expect.any(String)});
   }
-  test('Basic functionality'), () => {
-    expect(channelListAllV1(user_id, channel_id)).toStrictEqual([{
+  test('Shows all public channels'), () => {
+    expect(channelListAllV1(user_id)).toStrictEqual([{
         channelId: channel_id,
         name: 'COMP1531'
+    }, {
+      channelId: second_channel_id,
+      name: 'COMP2511'
+    }]);
+  }
+  test('Includes private with public channels'), () => {
+    const private_id = channelsCreateV1(user_id, 'COMP3311', false);
+    expect(channelListAllV1(user_id)).toStrictEqual([{
+      channelId: channel_id,
+      name: 'COMP1531'
+    }, {
+      channelId: second_channel_id,
+      name: 'COMP2511'
+    }, {
+      channeId: private_id,
+      name: 'COMP3311'
+    }]);
+  }
+  test('Includes channels user is not part of'), () => {
+    const second_user_id = authRegisterV1('yj@unsw.edu.au', 'PASSWORD', 'Yuchao', 'Jiang');
+    const private_id = channelsCreateV1(second_user_id, 'COMP3311', false);
+    expect(channelListAllV1(user_id)).toStrictEqual([{
+      channelId: channel_id,
+      name: 'COMP1531'
+    }, {
+      channelId: second_channel_id,
+      name: 'COMP2511'
+    }, {
+      channeId: private_id,
+      name: 'COMP3311'
     }]);
   }
 }
