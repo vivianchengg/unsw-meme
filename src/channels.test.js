5c9fb83e2d0555a576cfc5332ac1afc8ade6e112
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-08 13:52:44 +1100

    Added tests for channelsListAllV1

---
+++ src/channels.test.js
@@ -0,0 +1,22 @@
+import { channelsListAllV1 } from './channel.js';
+import { channelsCreateV1 } from './channels.js';
+import { authRegisterV1 } from './auth.js';
+import { clearV1 } from './other.js';
+
+beforeEach(() => {
+  clearV1();
+  let user_id = authRegisterV1('jr@unsw.edu.au', 'password', 'Jake', 'Renzella');
+  let channel_id = channelsCreateV1(user_id, 'COMP1531', true);
+});
+
+describe('channelListAllV1 Tests'), () => {
+  test('Invalid authUserId'), () => {
+    expect(channelsListAllV1(user_id + 1, channel_id)).toStrictEqual({ error: 'invalid authUserId' });
+  }
+  test('Basic functionality'), () => {
+    expect(channelListAllV1(user_id, channel_id)).toStrictEqual([{
+        channelId: channel_id,
+        name: 'COMP1531'
+    }]);
+  }
+}
