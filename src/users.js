3d2835c2b4ad47314a24f626adb96c6302e52be0
Christine <z5425742@ad.unsw.edu.au>
2023-03-09 23:12:48 +1100

    userProfile function

---
+++ src/users.js
@@ -0,0 +1,51 @@
+import { authRegisterV1 } from 'auth.js';
+
+/**
+ * Returns information about a user
+ * @param {number} authUserId
+ * @param {number} hobby
+ * @returns {{
+ * 	uId: number,
+ * 	email: string,
+ * 	nameFirst: string,
+ * 	nameLast: string,
+ * 	handleStr: string,
+ * }}
+ */
+export function userProfileV1 (authUserId, uId) {
+	const data = getData();
+
+	if (validate_user(authUserId) === false) {
+		return { error: 'invalid authUserId' };
+	}
+	if (validate_user(uId) === false) {
+		return { error: 'invalid uId' };
+	}
+
+	for (const user of data.users) {
+		if (user.uId === uId) {
+			const person = {
+				uId: user.uId,
+    		email: user.email,
+  			nameFirst: user.nameFirst,
+    		nameLast: user.nameLast,
+    		handleStr: user.handleStr,
+			}
+			
+		}
+	}
+	
+	return person;
+}
+
+// checks if user is valid
+function validate_user (user_id) {
+	const data = getData();
+	for (const user of data.users) {
+		if (user.userId === user_id) {
+			return true
+		}
+	}
+
+	return false
+}
