591771c9d5bd4b8526f295fdd680aebaa6ccdd89
Christine <z5425742@ad.unsw.edu.au>
2023-03-10 00:23:01 +1100

    fixed up userrrr

--- src/users.test.js
+++ src/users.test.js
@@ -8,9 +8,9 @@ const ERROR = { error: expect.any(String) };
 
 describe ('VALID INPUT!', () => {
   test('Test: valid authuserId and uId', () => {
-    const authUserId = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(authUserId.uId, authUserId.uId)).toStrictEqual({
-        uId: authUserId.id,
+    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
+    expect(userProfileV1(user.authUserId, authUserId.uId)).toStrictEqual({
+        uId: user.authUserId,
         email: 'christine@gmail.com',
         nameFirst: 'christine',
         nameLast: 'chu',
@@ -22,12 +22,12 @@ describe ('VALID INPUT!', () => {
 describe ('INVALID!', () => {
   test('Test: invalid authUserId', () => {
     const authUserId = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(3, authUserId.uId)).toStrictEqual({ERROR});
+    expect(userProfileV1(3, authUserId.uId)).toStrictEqual(ERROR);
   });
 
   test('Test: invalid authUserId', () => {
     const authUserId = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(authUserId.uId, 6)).toStrictEqual({ERROR});
+    expect(userProfileV1(authUserId.uId, 6)).toStrictEqual(ERROR);
   });
 
 });
