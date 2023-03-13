68e98cd408344aaecd8200ca65d0e262e976e068
Vivian <z5332415@ad.unsw.edu.au>
2023-03-14 02:27:40 +1100

    fix files

--- src/other.test.ts
+++ src/other.test.ts
@@ -1,10 +1,10 @@
-import { clearV1 } from './other.js';
-import { authRegisterV1 } from './auth.js';
-import { channelsCreateV1 } from './channels.js';
-import { userProfileV1 } from './users.js';
-import { channelDetailsV1 } from './channel.js';
+import { clearV1 } from './other';
+import { authRegisterV1 } from './auth';
+import { channelsCreateV1 } from './channels';
+import { userProfileV1 } from './users';
+import { channelDetailsV1 } from './channel';
 
-const ERROR = { error: expect.any(String)};
+const ERROR = { error: expect.any(String) };
 
 describe('Test clearV1 function', () => {
   test('test clearV1 - user and channel', () => {
@@ -15,10 +15,10 @@ describe('Test clearV1 function', () => {
     const channel = channelDetailsV1(user.authUserId, newChannel.channelId);
     expect(person).toStrictEqual(ERROR);
     expect(channel).toStrictEqual(ERROR);
-  })
+  });
 
   test('test clearV1 - basic output', () => {
     const result = clearV1();
     expect(result).toStrictEqual({});
-  }) 
-})
+  });
+});
