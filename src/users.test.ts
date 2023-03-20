804f41d9fc61453233e062068f6cdf4c51b7b963
Christine <z5425742@ad.unsw.edu.au>
2023-03-20 11:25:37 +1100

    users.test.ts - fixed up iteration 1 code (by moving const user into beforeeach to reduce repetitive code) also wrote http for userProfileV2 tests! yay!

--- src/users.test.ts
+++ src/users.test.ts
@@ -2,15 +2,75 @@ import { clearV1 } from './other';
 import { userProfileV1 } from './users';
 import { authRegisterV1 } from './auth';
 
+import request from 'sync-request';
+import config from './config.json';
+
+const OK = 200;
+const port = config.port;
+const url = config.url;
+
+//iteration 2
+const getRequestGET = (url: string, data: any) => {
+  const res = request('GET', url, { qs: data, });
+  const bodyObj = JSON.parse(String(res.getBody()));
+  return bodyObj;
+}
+
+//takes in token + uId
+describe ('HTTP - userProfileV2 tests', () => {
+  test('Testing valid token + uId', () => {
+    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
+    const bodyObj = getRequestGET(`${url}:${port}/user/profile/v2`, {
+      token: user.token[0],
+      id: user.uId,
+    });
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toEqual({
+      user: {
+        uId: user.authUserId,
+        email: 'christine@gmail.com',
+        nameFirst: 'christine',
+        nameLast: 'chu',
+        handleStr: 'christinechu',
+      }
+    });
+  })
+
+  test('Testing invalid token', () => {
+    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
+    const bodyObj = getRequestGET(`${url}:${port}/user/profile/v2`, {
+      token: user.token[0] + 'yay!',
+      id: user.uId,
+    });
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toEqual(ERROR);
+  })
+
+  test('Testing invalid uId', () => {
+    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
+    const bodyObj = getRequestGET(`${url}:${port}/user/profile/v2`, {
+      token: user.token[0],
+      id: user.uId + 1,
+    });
+    expect(res.statusCode).toBe(OK);
+    expect(bodyObj).toEqual(ERROR);
+  })
+});
+
+
+
+// iteration 1
+let user: any;
+
 beforeEach(() => {
   clearV1();
+  const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
 });
 
 const ERROR = { error: expect.any(String) };
 
 describe('VALID INPUT!', () => {
   test('Test: valid authuserId and uId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
     expect(userProfileV1(user.authUserId, user.authUserId)).toStrictEqual({
       user: {
         uId: user.authUserId,
@@ -25,12 +85,10 @@ describe('VALID INPUT!', () => {
 
 describe('INVALID!', () => {
   test('Test: invalid authUserId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
     expect(userProfileV1(user.authUserId + 1, user.authUserId)).toStrictEqual(ERROR);
   });
 
   test('Test: invalid authUserId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
     expect(userProfileV1(user.authUserId, user.authUserId + 1)).toStrictEqual(ERROR);
   });
 });
