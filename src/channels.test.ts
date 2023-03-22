9f02b0373041cb55394eadb690c71362118a5ce3
Christine <z5425742@ad.unsw.edu.au>
2023-03-22 22:23:22 +1100

    npm run tsc - fixed up user to be type any + npm run lint added line to end of file

--- src/channels.test.ts
+++ src/channels.test.ts
@@ -7,6 +7,8 @@ const url = config.url;
 const ERROR = { error: expect.any(String) };
 const SERVERurl = `${url}:${port}`;
 
+let user : any;
+
 const postRequest = (url: string, data: any) => {
   const res = request('POST', SERVERurl + url, { json: data });
   const body = JSON.parse(String(res.getBody()));
@@ -18,8 +20,6 @@ const deleteRequest = (url: string, data: any) => {
   const body = JSON.parse(String(res.getBody()));
   return body;
 };
-
-let user;
 
 beforeEach(() => {
   deleteRequest('/clear/v1', {});
@@ -72,75 +72,5 @@ describe('HTTP - channelsCreateV2 Tests', () => {
     };
     const channelId = postRequest('/channels/create/v2', param);
     expect(channelId).toStrictEqual(ERROR);
-  });
-});
-
-describe('channelsListV1 Tests', () => {
-  test('Test: invalid authUserId', () => {
-    expect(channelsListV1(user.authUserId + 1)).toStrictEqual(ERROR);
-  });
-
-  test('Valid authUserId', () => {
-    expect(channelsListV1(user.authUserId)).toStrictEqual({
-      channels: [{
-        channelId: channel.channelId,
-        name: 'COMP1531',
-      }]
-    });
-  });
-});
-
-describe('channelListAllV1 Tests', () => {
-  test('Invalid authUserId', () => {
-    channelsCreateV1(user.authUserId, 'COMP1531', true);
-    expect(channelsListAllV1(user.authUserId + 1)).toStrictEqual(ERROR);
-  });
-  test('Basic functionality', () => {
-    const channel2 = channelsCreateV1(user.authUserId, 'COMP2511', true);
-    expect(channelsListAllV1(user.authUserId)).toStrictEqual({
-      channels:
-      [{
-        channelId: channel.channelId,
-        name: 'COMP1531'
-      }, {
-        channelId: channel2.channelId,
-        name: 'COMP2511'
-      }]
-    });
-  });
-  test('Includes private with public channels', () => {
-    const channel2 = channelsCreateV1(user.authUserId, 'COMP2511', true);
-    const channelPriv = channelsCreateV1(user.authUserId, 'COMP3311', false);
-    expect(channelsListAllV1(user.authUserId)).toStrictEqual({
-      channels:
-      [{
-        channelId: channel.channelId,
-        name: 'COMP1531'
-      }, {
-        channelId: channel2.channelId,
-        name: 'COMP2511'
-      }, {
-        channelId: channelPriv.channelId,
-        name: 'COMP3311'
-      }]
-    });
-  });
-  test('Includes channels user is not part of', () => {
-    const user2 = authRegisterV1('yj@unsw.edu.au', 'PASSWORD', 'Yuchao', 'Jiang');
-    const channel2 = channelsCreateV1(user.authUserId, 'COMP2511', true);
-    const channelPriv = channelsCreateV1(user2.authUserId, 'COMP3311', false);
-    expect(channelsListAllV1(user.authUserId)).toStrictEqual({
-      channels:
-    [{
-      channelId: channel.channelId,
-      name: 'COMP1531'
-    }, {
-      channelId: channel2.channelId,
-      name: 'COMP2511'
-    }, {
-      channelId: channelPriv.channelId,
-      name: 'COMP3311'
-    }]
-    });
   });
 });
