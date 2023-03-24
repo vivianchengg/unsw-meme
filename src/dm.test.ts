2d7184909376482b6af978f0832b61085a2e1523
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-24 13:41:26 +1100

    Began tests for dm/details/v1

---
+++ src/dm.test.ts
@@ -0,0 +1,129 @@
+import request from 'sync-request';
+import { port, url } from './config.json';
+import { getData } from './dataStore';
+
+const SERVER_URL = `${url}:${port}`;
+const ERROR = { error: expect.any(String) };
+
+const getRequest = (url: string, data: any) => {
+  const res = request(
+    'GET',
+    SERVER_URL + url,
+    {
+      qs: data,
+    }
+  );
+  const body = JSON.parse(res.getBody() as string);
+  return body;
+};
+
+const postRequest = (url: string, data: any) => {
+  const res = request(
+    'POST',
+    SERVER_URL + url,
+    {
+      json: data,
+    }
+  );
+  const body = JSON.parse(res.getBody() as string);
+  return body;
+};
+
+const deleteRequest = (url: string, data: any) => {
+  const res = request(
+    'DELETE',
+    SERVER_URL + url,
+    {
+      qs: data,
+    }
+  );
+  const body = JSON.parse(res.getBody() as string);
+  return body;
+};
+
+let user: any;
+let dm: any;
+
+describe('dmDetailsV1 Test', () => {
+  beforeEach(() => {
+    deleteRequest('/clear/v1', {});
+
+    const userData = {
+      email: 'jr@unsw.edu.au',
+      password: 'password',
+      nameFirst: 'Jake',
+      nameLast: 'Renzella'
+    };
+
+    user = postRequest('/auth/register/v2', userData);
+
+    const dmData = {
+      token: user.token,
+      uIds: user.authUserId
+    };
+
+    dm = postRequest('/dm/create/v1', dmData);
+  });
+
+  test('Invalid token', () => {
+    const detailRequest = {
+      token: '',
+      dmId: dm.dmId
+    };
+
+    expect(getRequest('/dm/details/v1', detailRequest)).toStrictEqual(ERROR);
+  });
+
+  test('Invalid dmId', () => {
+    const detailRequest = {
+      token: user.token,
+      dmId: dm.dmId + 1
+    };
+
+    expect(getRequest('/dm/details/v1', detailRequest)).toStrictEqual(ERROR);
+  });
+
+  test('Valid dmId but user not member of DM', () => {
+    const user2Data = {
+      email: 'yj@unsw.edu.au',
+      password: 'PASSWORD',
+      nameFirst: 'Yuchao',
+      nameLast: 'Jiang'
+    };
+
+    const user2 = postRequest('/auth/register/v2', user2Data);
+
+    const detailRequest = {
+      token: user2.token,
+      dmId: dm.dmId
+    };
+
+    expect(getRequest('/dm/details/v1', detailRequest)).toStrictEqual(ERROR);
+  });
+
+  test('Basic functionality', () => {
+    const detailRequest = {
+      token: user.token,
+      dmId: dm.dmId
+    };
+
+    const data = getData();
+    let dmName: any;
+    for (const dmDetail of data.dms) {
+      if (dmDetail.dmId === dm.dmId) {
+        dmName = dmDetail.name;
+      }
+    }
+
+    expect(getRequest('/dm/details/v1', detailRequest)).toStrictEqual({
+      name: dmName,
+      members: [{
+        uId: user.uId,
+        email: user.email,
+        nameFirst: user.nameFirst,
+        nameLast: user.nameLast,
+        handleStr: user.handleStr
+      }]
+    });
+  });
+});
