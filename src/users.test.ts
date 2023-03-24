7fc1ce427bd8d02723d72823521492449b2babd9
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-24 17:15:07 +1100

    Added tests for profile/setemail

--- src/users.test.ts
+++ src/users.test.ts
@@ -1,36 +1,105 @@
-import { clearV1 } from './other';
-import { userProfileV1 } from './users';
-import { authRegisterV1 } from './auth';
+import request from 'sync-request';
+import config from './config.json';
 
-beforeEach(() => {
-  clearV1();
-});
+const port = config.port;
+const url = config.url;
 
 const ERROR = { error: expect.any(String) };
+const SERVER = `${url}:${port}`;
 
-describe('VALID INPUT!', () => {
-  test('Test: valid authuserId and uId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(user.authUserId, user.authUserId)).toStrictEqual({
-      user: {
-        uId: user.authUserId,
-        email: 'christine@gmail.com',
-        nameFirst: 'christine',
-        nameLast: 'chu',
-        handleStr: 'christinechu',
-      }
-    });
-  });
+let user: any;
+
+const postRequest = (url: string, data: any) => {
+  const res = request(
+    'POST',
+    SERVER + url,
+    {
+      json: data,
+    }
+  );
+  const body = JSON.parse(res.getBody() as string);
+  return body;
+};
+
+const putRequest = (url: string, data: any) => {
+  const res = request(
+    'PUT',
+    SERVER + url,
+    {
+      json: data,
+    }
+  );
+  const body = JSON.parse(res.getBody() as string);
+  return body;
+};
+
+const deleteRequest = (url: string, data: any) => {
+  const res = request(
+    'DELETE',
+    SERVER + url,
+    {
+      qs: data,
+    }
+  );
+  const body = JSON.parse(res.getBody() as string);
+  return body;
+};
+
+beforeEach(() => {
+  deleteRequest('/clear/v1', {});
+  const person = {
+    email: 'jr@unsw.edu.au',
+    password: 'password',
+    nameFirst: 'Jake',
+    nameLast: 'Renzella'
+  };
+
+  user = postRequest('/auth/register/v2', person);
 });
 
-describe('INVALID!', () => {
-  test('Test: invalid authUserId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(user.authUserId + 1, user.authUserId)).toStrictEqual(ERROR);
+describe('userProfileSetEmailV1 tests', () => {
+  test('Invalid token', () => {
+    const param = {
+      token: user.token + 'buffer',
+      email: 'jake23@unsw.edu.au'
+    };
+
+    expect(putRequest('/user/profile/setemail/v1', param)).toStrictEqual(ERROR);
   });
 
-  test('Test: invalid authUserId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(user.authUserId, user.authUserId + 1)).toStrictEqual(ERROR);
+  test('Invalid email', () => {
+    const param = {
+      token: user.token,
+      email: 'buffer'
+    };
+
+    expect(putRequest('/user/profile/setemail/v1', param)).toStrictEqual(ERROR);
+  });
+
+  test('Email already taken', () => {
+    const person2 = {
+      email: 'yj@unsw.edu.au',
+      password: 'PASSWORD',
+      nameFirst: 'Yuchao',
+      nameLast: 'Jiang'
+    };
+
+    const user2 = postRequest('/auth/register/v2', person2);
+
+    const param = {
+      token: user.token,
+      email: user2.email
+    };
+
+    expect(putRequest('/user/profile/setemail/v1', param)).toStrictEqual(ERROR);
+  });
+
+  test('Invalid token', () => {
+    const param = {
+      token: user.token,
+      email: 'jake23@unsw.edu.au'
+    };
+
+    expect(putRequest('/user/profile/setemail/v1', param)).toStrictEqual({});
   });
 });
