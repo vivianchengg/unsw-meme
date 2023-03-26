bb7b99357d8e8bd0c9394093e21951f1e92687cb
Vivian <z5332415@ad.unsw.edu.au>
2023-03-27 03:34:51 +1100

    update doc

--- src/channel.ts
+++ src/channel.ts
@@ -315,7 +315,7 @@ const isChannelOwner = (uId: number, channel: Channel) => {
 };
 
 /**
-  * check whether email entered belong to a user
+  * Make user with user id uId an owner of the channel.
   *
   * @param {string} token
   * @param {number} channelId
