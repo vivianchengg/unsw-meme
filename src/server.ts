33b21bcc0bc8ab4c142fc6731224c2ba8fd8f15d
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-27 13:05:10 +1100

    Added HTTP path for userProfileSetHandle

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
 import cors from 'cors';
+import userProfileSetHandle from './users';
 
 // Set up web app
 const app = express();
@@ -20,6 +21,11 @@ const HOST: string = process.env.IP || 'localhost';
 app.get('/echo', (req: Request, res: Response, next) => {
   const data = req.query.echo as string;
   return res.json(echo(data));
+});
+
+app.put('/user/profile/sethandle/v1', (req: Request, res: Response) => {
+  const { token, handleStr } = req.body;
+  return res.json(userProfileSetHandle(token, handleStr));
 });
 
 // start server
