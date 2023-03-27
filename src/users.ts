45aeb263af3dd01e2f06ccb90568b8649c89133d
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-27 13:22:00 +1100

    Added function for userProfileSetEmailV1

--- src/users.ts
+++ src/users.ts
@@ -1,4 +1,5 @@
 import { getData } from './dataStore';
+import validator from 'validator';
 
 /**
 * Returns information about a user
@@ -53,4 +54,85 @@ const isValidUser = (userId: number): boolean => {
     }
   }
   return false;
+};
+
+/**
+  * Checks if the token is valid
+  * @param {string} token
+  * ...
+  * @returns {boolean}
+*/
+const isValidToken = (token: string): boolean => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.token.includes(token)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+  * Finds the authUserId given a token.
+  * @param {string} token
+  * ...
+  * @returns {string} authUserId
+*/
+const findUID = (token: string) => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.token.includes(token)) {
+      return user.uId;
+    }
+  }
+  return null;
+};
+
+/**
+ * Updates authorised user's email address
+ * @param {string} token
+ * @param {string} email
+ * @returns {}
+ *
+ * Will return error if:
+ *  - email entered is not valid, defined by validator
+ *  - email is already taken by another user
+ *  - token is invalid
+ */
+export const userProfileSetEmailV1 = (token: string, email: string) => {
+  if (!isValidToken(token)) {
+    return { error: 'Invalid token' };
+  }
+
+  if (!validator.isEmail(email)) {
+    return { error: 'Invalid email entered' };
+  }
+
+  if (!isEmailFree(email)) {
+    return { error: 'Email already taken by another user' };
+  }
+
+  const data = getData();
+  const userId = findUID(token);
+  for (const user of data.users) {
+    if (user.uId === userId) {
+      user.email = email;
+    }
+  }
+};
+
+/**
+ * Checks if another user already has the inputted email
+ * @param {string} email
+ * @returns {boolean}
+ */
+const isEmailFree = (email: string): boolean => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.email === email) {
+      return false;
+    }
+  }
+
+  return true;
 };
