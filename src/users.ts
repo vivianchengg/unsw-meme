8b284d6face20922963a490b46d523fa37f3ae80
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-27 13:49:24 +1100

    Coded userProfileSetHandleV1

--- src/users.ts
+++ src/users.ts
@@ -54,3 +54,100 @@ const isValidUser = (userId: number): boolean => {
   }
   return false;
 };
+
+/**
+  * Checks if the token is valid
+  * @param {string} token
+  * ...
+  * @returns {boolean}
+*/
+const isValidToken = (token: string): boolean => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.token.includes(token)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+  * Finds the authUserId given a token.
+  * @param {string} token
+  * ...
+  * @returns {string} authUserId
+*/
+const findUID = (token: string) => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.token.includes(token)) {
+      return user.uId;
+    }
+  }
+  return null;
+};
+
+/**
+ * Updates authorised user's handle
+ * @param {string} token
+ * @param {string} handleStr
+ * @returns {}
+ *
+ * Will return error if:
+ *  - Length of handleStr is not between 3-20 characters
+ *  - Non-alphanumeric characters are contained in handleStr
+ *  - handleStr is already taken as handle for another user
+ *  - token is invalid
+ */
+export const userProfileSetHandleV1 = (token: string, handleStr: string) => {
+  if (!isValidToken(token)) {
+    return { error: 'Invalid token' };
+  }
+
+  const minimumLength = 3;
+  const maximumLength = 20;
+  if (handleStr.length > maximumLength || handleStr.length < minimumLength) {
+    return { error: 'Length of handleStr is not between 3-20 characters' };
+  }
+
+  if (!isHandleFree(handleStr)) {
+    return { error: 'Handle already taken by another user' };
+  }
+
+  if (!isAlphanumeric(handleStr)) {
+    return { error: 'Handle contains non-alphanumeric characters' };
+  }
+
+  const data = getData();
+  const userId = findUID(token);
+  for (const user of data.users) {
+    if (user.uId === userId) {
+      user.handleStr = handleStr;
+    }
+  }
+};
+
+/**
+ * Checks if another user already has the inputted handle
+ * @param {string} handleStr
+ * @returns {boolean}
+ */
+const isHandleFree = (handleStr: string): boolean => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.handleStr === handleStr) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+/**
+ * Checks if entered string is alphanumeric
+ * @param {string} str
+ * @returns {boolean}
+ */
+const isAlphanumeric = (str: string): boolean => {
+  return /^[a-zA-Z0-9]+$/.test(str);
+};
