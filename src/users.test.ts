aa23a02cd5ee8d29fefbc1a10ebe71d095bd10ac
Christine <z5425742@ad.unsw.edu.au>
2023-03-27 16:14:21 +1100

    written test code for users profile set name v1

--- src/users.test.ts
+++ src/users.test.ts
@@ -1,36 +1,80 @@
-import { clearV1 } from './other';
-import { userProfileV1 } from './users';
-import { authRegisterV1 } from './auth';
+import request from 'sync-request';
+import config from './config.json';
 
-beforeEach(() => {
-  clearV1();
-});
+const port = config.port;
+const url = config.url;
 
 const ERROR = { error: expect.any(String) };
+const SERVERurl = `${url}:${port}`;
 
-describe('VALID INPUT!', () => {
-  test('Test: valid authuserId and uId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(user.authUserId, user.authUserId)).toStrictEqual({
-      user: {
-        uId: user.authUserId,
-        email: 'christine@gmail.com',
-        nameFirst: 'christine',
-        nameLast: 'chu',
-        handleStr: 'christinechu',
-      }
-    });
-  });
+const postRequest = (url: string, data: any) => {
+  const res = request('POST', SERVERurl + url, { json: data });
+  const body = JSON.parse(String(res.getBody()));
+  return body;
+};
+
+const deleteRequest = (url: string, data: any) => {
+  const res = request('DELETE', SERVERurl + url, { qs: data });
+  const body = JSON.parse(String(res.getBody()));
+  return body;
+};
+
+const getRequest = (url: string, data: any) => {
+  const res = request('GET', SERVERurl + url, { qs: data });
+  const body = JSON.parse(String(res.getBody()));
+  return body;
+};
+
+const putRequest = (url: string, data: any) => {
+  const res = request('PUT', SERVERurl + url, { json: data });
+  const body = JSON.parse(String(res.getBody()));
+  return body;
+};
+
+let user: any;
+
+beforeEach(() => {
+  deleteRequest('/clear/v1', {});
+  const person = {
+    email: 'jr@unsw.edu.au',
+    password: 'password',
+    nameFirst: 'Jake',
+    nameLast: 'Renzella'
+  };
+  user = postRequest('/auth/register/v2', person);
 });
 
-describe('INVALID!', () => {
-  test('Test: invalid authUserId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(user.authUserId + 1, user.authUserId)).toStrictEqual(ERROR);
+describe('HTTP - /user/profile/setname/v1', () => {
+  test('Invalid token', () => {
+    const param = {
+      token: user.token[0] - 1,
+      nameFirst: 'yum',
+      nameLast: 'my',
+    };
+    expect(putRequest('/user/profile/setname/v1', param)).toStrictEqual(ERROR);
   });
 
-  test('Test: invalid authUserId', () => {
-    const user = authRegisterV1('christine@gmail.com', 'password', 'christine', 'chu');
-    expect(userProfileV1(user.authUserId, user.authUserId + 1)).toStrictEqual(ERROR);
+  test('Valid input', () => {
+    const param = {
+      token: user.token[0],
+      nameFirst: 'yum',
+      nameLast: 'my',
+    };
+    putRequest('/user/profile/setname/v1', param);
+
+    const newparam = {
+      token: user.token[0],
+      uId: user.authUserId,
+    };
+    const details = getRequest('/user/profile/v2', newparam);
+    expect(details).toStrictEqual({
+      user: {
+        uId: details.uId,
+        email: details.email,
+        nameFirst: 'yum',
+        nameLast: 'my',
+        handleStr: 'yummy',
+      }
+    });
   });
 });
