05d4ccef796cba5b31252fb1f733376e43169c41
Christine <z5425742@ad.unsw.edu.au>
2023-03-27 16:26:02 +1100

    written route for user profile set name

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
 import cors from 'cors';
+import { userProfileSetName } from './users';
 
 // Set up web app
 const app = express();
@@ -20,6 +21,11 @@ const HOST: string = process.env.IP || 'localhost';
 app.get('/echo', (req: Request, res: Response, next) => {
   const data = req.query.echo as string;
   return res.json(echo(data));
+});
+
+app.put('/user/profile/setname/v1', (req: Request, res: Response) => {
+  const { token, nameFirst, nameLast } = req.body as string;
+  return res.json(userProfileSetName(token, nameFirst, nameLast));
 });
 
 // start server
