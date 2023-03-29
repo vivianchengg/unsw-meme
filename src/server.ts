01589d10be73952b0d64d8859aa2976fa07a24ca
Dulini <z5421898@ad.unsw.edu.au>
2023-03-29 16:33:35 +1100

    adding HTML route for messageRemoveV1

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { json, Request, Response } from 'express';
-import { echo } from './echo';
+import { messageRemoveV1 } from './message';
 import morgan from 'morgan';
 import config from './config.json';
 import cors from 'cors';
@@ -16,10 +16,9 @@ app.use(morgan('dev'));
 const PORT: number = parseInt(process.env.PORT || config.port);
 const HOST: string = process.env.IP || 'localhost';
 
-// Example get request
-app.get('/echo', (req: Request, res: Response, next) => {
-  const data = req.query.echo as string;
-  return res.json(echo(data));
+app.delete('/message/remove/v1', (req: Request, res: Response, next) => {
+  const { token, messageId } = req.query;
+  res.json(messageRemoveV1(token, messageId));
 });
 
 // start server
