bd74da5fc56aa393d48563070695dc3b17e0b383
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-03-29 21:30:31 +1100

    Fixed linting errors

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 // YOU SHOULD MODIFY THIS OBJECT BELOW
 export type User = {
   uId: number,
@@ -7,7 +9,7 @@ export type User = {
   handleStr: string,
   password: string,
   pId: number,
-  tokens: [],
+  token: string[],
 };
 
 export type Message = {
@@ -44,12 +46,11 @@ export type Data = {
   dms: Dm[]
 };
 
-let data: Data = {
-  users: [],
-  channels: [],
-  dms: []
-};
-
+// let data: Data = {
+//   users: [],
+//   channels: []
+//   dms: []
+// };
 // YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
 
 /*
@@ -68,6 +69,8 @@ Example usage
 
 // Use get() to access the data
 export const getData = (): Data => {
+  const json = fs.readFileSync('./src/data.json', { flag: 'r' });
+  const data = JSON.parse(json.toString());
   return data;
 };
 
@@ -76,5 +79,6 @@ export const getData = (): Data => {
 // - Javascript uses pass-by-reference for objects... read more here: https://stackoverflow.com/questions/13104494/does-javascript-pass-by-reference
 // Hint: this function might be useful to edit in iteration 2
 export const setData = (newData: Data) => {
-  data = newData;
+  const dataString = JSON.stringify(newData);
+  fs.writeFileSync('./src/data.json', dataString, { flag: 'w' });
 };
--- src/users.ts
+++ src/users.ts
@@ -1,8 +1,9 @@
-import { getData } from './dataStore';
+import { getData, setData } from './dataStore';
+import validator from 'validator';
 
 /**
 * Returns information about a user
-* @param {number} authUserId
+* @param {string} token
 * @param {number} uId
 * ...
 * @returns {{
@@ -14,12 +15,19 @@ import { getData } from './dataStore';
 * }} user
 */
 
-export const userProfileV1 = (authUserId: number, uId: number) => {
+export const userProfileV1 = (token: string, uId: number) => {
   const data = getData();
-  if (isValidUser(authUserId) === false) {
+
+  if (isValidToken(token) === false) {
+    return { error: 'invalid token' };
+  }
+
+  const authUserId = findUID(token);
+
+  if (!isValidUser(authUserId)) {
     return { error: 'invalid authUserId' };
   }
-  if (isValidUser(uId) === false) {
+  if (!isValidUser(uId)) {
     return { error: 'invalid uId' };
   }
 
@@ -35,9 +43,41 @@ export const userProfileV1 = (authUserId: number, uId: number) => {
       };
     }
   }
+
   return {
-    user: person,
+    user: person
   };
+};
+
+/**
+* Changes the first and last name of user
+* @param {string} token
+* @param {string} nameFirst
+* @param {string} nameLast
+* ...
+* @returns {}
+  */
+export const userProfileSetName = (token: string, nameFirst: string, nameLast: string) => {
+  const data = getData();
+
+  if (!invalidName(nameLast)) {
+    return { error: 'name length +51 or less than 1' };
+  }
+  if (!invalidName(nameFirst)) {
+    return { error: 'name length +51 or less than 1' };
+  }
+
+  const authUserId = extractUId(token);
+  if (authUserId === undefined) {
+    return { error: 'invalid token' };
+  }
+
+  const user = data.users.find(d => d.uId === authUserId);
+  user.nameFirst = nameFirst;
+  user.nameLast = nameLast;
+
+  setData(data);
+  return {};
 };
 
 /**
@@ -53,4 +93,159 @@ const isValidUser = (userId: number): boolean => {
     }
   }
   return false;
+};
+
+/** Function that returns user Id from token
+ *
+ * @param {string} token
+ * @returns {number}
+ */
+const extractUId = (token: string) => {
+  const data = getData();
+  let userId;
+
+  for (const user of data.users) {
+    for (const tokenData of user.token) {
+      if (tokenData === token) {
+        userId = user.uId;
+      }
+    }
+  }
+
+  return userId;
+};
+
+/**
+  * Checks if name last and name first is valid length
+  * @param {string} nameLast
+  * ...
+  * @returns {boolean}
+*/
+const invalidName = (name: string) => {
+  const length = name.length;
+  const min = 1;
+  const max = 50;
+  if (length < min || length > max) {
+    return false;
+  }
+  return true;
+};
+
+/**
+  * Checks if the token is valid
+  * @param {string} token
+  * ...
+  * @returns {boolean}
+*/
+const isValidToken = (token: string): boolean => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.token.includes(token)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+  * Finds the authUserId given a token.
+  * @param {string} token
+  * ...
+  * @returns {string} authUserId
+*/
+const findUID = (token: string) => {
+  const data = getData();
+  for (const user of data.users) {
+    if (user.token.includes(token)) {
+      return user.uId;
+    }
+  }
+  return null;
+};
+
+/**
+<<<<<<< HEAD
+ * Updates authorised user's handle
+ * @param {string} token
+ * @param {string} handleStr
+ * @returns {}
+ *
+ * Will return error if:
+ *  - Length of handleStr is not between 3-20 characters
+ *  - Non-alphanumeric characters are contained in handleStr
+ *  - handleStr is already taken as handle for another user
+ *  - token is invalid
+ */
+export const userProfileSetHandleV1 = (token: string, handleStr: string) => {
+  const data = getData();
+
+  if (!isValidToken(token)) {
+    return { error: 'Invalid token' };
+  }
+
+  const minimumLength = 3;
+  const maximumLength = 20;
+  if (handleStr.length > maximumLength || handleStr.length < minimumLength) {
+    return { error: 'Length of handleStr is not between 3-20 characters' };
+  }
+
+  if (data.users.find(u => u.handleStr === handleStr) !== undefined) {
+    return { error: 'Handle already taken by another user' };
+  }
+
+  if (!isAlphanumeric(handleStr)) {
+    return { error: 'Handle contains non-alphanumeric characters' };
+  }
+
+  const userId = findUID(token);
+  const user = data.users.find(u => u.uId === userId);
+
+  user.handleStr = handleStr;
+  setData(data);
+
+  return {};
+};
+
+/**
+* Updates authorised user's email address
+* @param {string} token
+* @param {string} email
+* @returns {}
+*
+* Will return error if:
+*  - email entered is not valid, defined by validator
+*  - email is already taken by another user
+*  - token is invalid
+*/
+export const userProfileSetEmailV1 = (token: string, email: string) => {
+  const data = getData();
+
+  if (!isValidToken(token)) {
+    return { error: 'Invalid token' };
+  }
+
+  if (!validator.isEmail(email)) {
+    return { error: 'Invalid email entered' };
+  }
+
+  if (data.users.find(u => u.email === email) !== undefined) {
+    return { error: 'Email already taken by another user' };
+  }
+
+  const userId = findUID(token);
+  const user = data.users.find(u => u.uId === userId);
+
+  user.email = email;
+  setData(data);
+
+  return {};
+};
+
+/**
+ * Checks if entered string is alphanumeric
+ * @param {string} str
+ * @returns {boolean}
+ */
+const isAlphanumeric = (str: string): boolean => {
+  return /^[a-zA-Z0-9]+$/.test(str);
 };
