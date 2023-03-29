27ee033a3864147ac097951d53dcd2eb3c8ce10a
Dulini <z5421898@ad.unsw.edu.au>
2023-03-29 16:09:22 +1100

    added HTML route for messageSendV1

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express, { json, Request, Response } from 'express';
-import { echo } from './echo';
+import { messageSendV1 } from './message';
 import morgan from 'morgan';
 import config from './config.json';
 import cors from 'cors';
+import { channelDetailsV1, channelJoinV1 } from './channel';
+import { channelsCreateV1 } from './channels';
+import { clearV1 } from './other';
+import { userProfileV1 } from './users';
+import { authRegisterV1, authLoginV1 } from './auth';
 
 // Set up web app
 const app = express();
@@ -16,10 +21,45 @@ app.use(morgan('dev'));
 const PORT: number = parseInt(process.env.PORT || config.port);
 const HOST: string = process.env.IP || 'localhost';
 
-// Example get request
-app.get('/echo', (req: Request, res: Response, next) => {
-  const data = req.query.echo as string;
-  return res.json(echo(data));
+app.post('/message/send/v1', (req: Request, res: Response, next) => {
+  const { token, channelId, message } = req.body;
+  res.json(messageSendV1(token, channelId, message));
+});
+
+app.delete('/clear/v1', (req: Request, res: Response) => {
+  return res.json(clearV1());
+});
+
+app.post('/auth/login/v2', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  return res.json(authLoginV1(email, password));
+});
+
+app.post('/auth/register/v2', (req: Request, res: Response) => {
+  const { email, password, nameFirst, nameLast } = req.body;
+  return res.json(authRegisterV1(email, password, nameFirst, nameLast));
+});
+
+app.get('/user/profile/v2', (req: Request, res: Response) => {
+  const token = req.query.token as string;
+  const uId = parseInt(req.query.uId as string);
+  return res.json(userProfileV1(token, uId));
+});
+
+app.post('/channels/create/v2', (req: Request, res: Response) => {
+  const { token, name, isPublic } = req.body;
+  return res.json(channelsCreateV1(token, name, isPublic));
+});
+
+app.get('/channel/details/v2', (req: Request, res: Response) => {
+  const token = req.query.token as string;
+  const channelId = parseInt(req.query.channelId as string);
+  return res.json(channelDetailsV1(token, channelId));
+});
+
+app.post('/channel/join/v2', (req: Request, res: Response, next) => {
+  const { token, channelId } = req.body;
+  return res.json(channelJoinV1(token, channelId));
 });
 
 // start server
