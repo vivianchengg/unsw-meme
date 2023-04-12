c4b48e0447eeb35110316f991eba8581d2ede895
Nathan Tan <z5420877@ad.unsw.edu.au>
2023-04-12 11:26:42 +1000

    Uncommented requestHelper functions

--- src/other.test.ts
+++ src/other.test.ts
@@ -23,55 +23,34 @@ describe('Test clearV1 function', () => {
     // const user = requestHelper('POST', '/auth/register/v3', {}, userData);
 
     // new channel
+    const tokenData = {
+      token: user.token
+    };
     const channelData = {
       token: user.token,
       name: 'ABC',
       isPublic: true
     };
-    const channel = postRequest('/channels/create/v2', channelData);
-
-    // const tokenData = {
-    //   token: user.token
-    // };
-    // const channelData = {
-    //   token: user.token,
-    //   name: 'ABC',
-    //   isPublic: true
-    // };
-    // const channel = requestHelper('POST', '/channels/create/v3', tokenData, channelData);
+    const channel = requestHelper('POST', '/channels/create/v3', tokenData, channelData);
 
     // clear
     requestHelper('DELETE', '/clear/v1', {}, {});
 
     // get user profile
+    const tokenData = {
+      token: user.token
+    };
     const profileData = {
-      token: user.token,
       uId: user.authUserId,
     };
 
     // get channel detail
     const detailData = {
-      token: user.token,
       channelId: channel.channelId,
     };
 
-    expect(getRequest('/user/profile/v2', profileData)).toStrictEqual(ERROR);
-    expect(getRequest('/channel/details/v2', detailData)).toStrictEqual(ERROR);
-
-    // const tokenData = {
-    //   token: user.token
-    // };
-    // const profileData = {
-    //   uId: user.authUserId,
-    // };
-
-    // // get channel detail
-    // const detailData = {
-    //   channelId: channel.channelId,
-    // };
-
-    // expect(() => requestHelper('GET', '/user/profile/v3', tokenData, profileData)).toThrow(Error);
-    // expect(() => requestHelper('GET', '/channel/details/v3', tokenData, detailData)).toThrow(Error);
+    expect(() => requestHelper('GET', '/user/profile/v3', tokenData, profileData)).toThrow(Error);
+    expect(() => requestHelper('GET', '/channel/details/v3', tokenData, detailData)).toThrow(Error);
   });
 
   test('test clearV1 - basic output', () => {
